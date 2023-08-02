0dd56aa9875ead2c4799339659595ffbe86fbd0c
Morteza <morteza.ahmadianmanzary@gmail.com>
2023-08-02 21:01:24 +0200

    SecondCommit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react';
+import { data } from './data';
+
 
 function App() {
+
+  const [name, setName] = useState(data);
+
+  const selectItem = (id) => {
+    const newItem = name.filter((item)=> item.id === id);
+    setName(newItem)
+  }
+
+  const removeItem = (id) => {
+    const newItem = name.filter((item)=> item.id !== id);
+    setName(newItem)
+  }
+
+  const [person, setPerson] = useState({
+    name:"morteza",
+    age: 36,
+    message: "Hello"
+  });
+
+  const changeMsg = () => {
+    setPerson({...person, message:"Develop everyday..."})
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      {
+        name.map((item) => { 
+          const {id, name} = item;
+          return (
+            <div className="list" key={id}>
+              <h2>{name}</h2>
+              <div className="btnArrange">
+                <button className="select" onClick={()=>selectItem(id)}>Select this one</button>
+                <button className="remove" onClick={()=>removeItem(id)}>Remove Item</button>
+              </div>
+            </div>
+          );
+
+        })
+      }
+      <button className="btn" onClick={()=>setName([])}>ClearList</button>
+      <h2>{person.name}</h2>
+      <h2>{person.age}</h2>
+      <h2>{person.message}</h2>
+      <button className="btn" onClick={changeMsg}>Change Message</button>
+    </>
   );
 }
 
---
+++ src/data.js
@@ -0,0 +1,19 @@
+export const data = [
+    {
+        id : 1,
+        name :"Morteza",
+    }, 
+    {
+        id: 2,
+        name: "Mahnaz",
+    },
+    {
+        id: 3,
+        name: "Narges",
+    },
+    {
+        id: 4,
+        name: "Pasha"
+    }
+
+]
